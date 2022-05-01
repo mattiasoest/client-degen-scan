8e5a09547841dcb461b163ed40780c87de662c30
Mattias <osth.mattias@gmail.com>
2022-05-01 16:52:01 +0900

    dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SocketProvider } from "../components/contexts/SocketProvider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#00FF00",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SocketProvider>
-      <Component {...pageProps} />
-    </SocketProvider>
+    <ThemeProvider theme={darkTheme}>
+      <SocketProvider>
+        <Component {...pageProps} />
+      </SocketProvider>
+    </ThemeProvider>
   );
 }
 
