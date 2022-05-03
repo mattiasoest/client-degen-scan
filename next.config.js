bdb8658f69010f153da85390433debf266c80eb6
Mattias <osth.mattias@gmail.com>
2022-05-03 19:57:36 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_WS_SERVER: process.env.NEXT_PUBLIC_WS_SERVER,
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
